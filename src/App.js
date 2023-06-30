1d51fde7fb0930efa83ba10092ad6416356d5fd4
anjul1026 <chaturvedianjul1899@gmail.com>
2023-06-30 07:32:24 -0700

    new commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/Navbar.Js';
+
+function App() {
+  return (
+  <>
+   <Navbar title="textutlis2" aboutText="about us"/>    
+  </>       
+  );
+}
+
+export default App;
+
